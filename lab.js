288b93a85e7bd0d11537a3f625172956b7b93347
Darren Wott <darren.wott@gmail.com>
2022-08-27 12:33:01 -0700

    solved problems

--- lab.js
+++ lab.js
@@ -140,16 +140,31 @@ while (score < passingScore){
 ////////// PROBLEM 9 //////////
 /*
   Create a variable called 'changeMyMind' and set it equal to true. 
-  Check to see if changeMyMind is set to true or false, if it is true, change the status to false, if it is false, change the status to true.
+  Check to see if changeMyMind is set to true or false, if it is true, 
+  change the status to false, if it is false, change the status to true.
 
 */
 
 //CODE HERE
+var changeMyMind = true
+
+if (changeMyMind === true){
+  changeMyMind = false
+} else if (changeMyMind === false){
+  changeMyMind = true
+}
+
+console.log(changeMyMind)
 
 ////////// PROBLEM 10 //////////
-// Using the "not" operator (!), change the current value of changeMyMind to true (it should currently be set to false due to the if-else statement in Problem 9). After you change the value of changeMyMind, console.log it's new value (it should now read true).
+// Using the "not" operator (!), change the current value of changeMyMind to true (it should currently be set to false due to the if-else statement in Problem 9).
+// After you change the value of changeMyMind, console.log it's new value (it should now read true).
+
 
 //CODE HERE
+changeMyMind = !changeMyMind
+console.log(changeMyMind)
+
 
 ////////// ADVANCED PROBLEMS //////////
 
@@ -162,3 +177,9 @@ let z = 5;
 //If your code is running properly, you should see: 5,4,3,2,1,4,3,2,1,3,2,1,2,1,1.
 
 //CODE HERE
+while(z > 0){
+  for(let i = z; i > 0; i--){
+    console.log(i)
+  }
+  z--
+}
